29fca81dde219ebe68f4ec244653dfc388c8c2c8
Yue Xin <yuecn41@gmail.com>
2015-08-09 20:59:35 +0800

    more pep8

--- timecalc.py
+++ timecalc.py
@@ -24,40 +24,42 @@ def sum_time(*time):
 def avg_time(time, divisor):
     hrs, mte = time
     total_time = hrs * TIME_UNIT + mte
-    ret = divmod( round(total_time * 1. / divisor), TIME_UNIT )
+    ret = divmod(round(total_time * 1. / divisor), TIME_UNIT)
     return map(int, ret)
 
-# hm :=
+
+def str2hm(arg):
+    # hm :=
     # '1/20'  <-> (1, 20)
     # '/20'   <-> (0, 20)
     # '1/'    <-> (1, 0)
-
-def str2hm(arg):
     hm = arg.split('/')
     assert len(hm) == 2
-    h,m = map(lambda x: int(x) if x else 0, hm)
+    h, m = map(lambda x: int(x) if x else 0, hm)
     assert m >= 0 and m < TIME_UNIT
     assert h >= 0
-    return (h,m)
+    return (h, m)
+
 
 def hm2str(arg):
     assert len(arg) == 2
     hm = map(lambda x: str(x) if x else '', arg)
     return '/'.join(hm)
 
+
 def arg_parse():
     parser = argparse.ArgumentParser()
-    subparsers = parser.add_subparsers(dest="mode",
-            help="which calculation mode to use")
+    subparsers = parser.add_subparsers(
+        dest="mode", help="which calculation mode to use")
 
     parser_sum = subparsers.add_parser("sum", help="summarize values of time")
-    parser_sum.add_argument("time", help="values of time to sumarize",
-            nargs="+", type=str2hm)
+    parser_sum.add_argument(
+        "time", help="values of time to sumarize", nargs="+", type=str2hm)
 
     parser_avg = subparsers.add_parser("avg", help="calculate average")
     parser_avg.add_argument("time", help="total time", type=str2hm)
-    parser_avg.add_argument("divisor", help="number to divide into",
-            nargs="+", type=int)
+    parser_avg.add_argument(
+        "divisor", help="number to divide into", nargs="+", type=int)
 
     args = parser.parse_args()
     return args
@@ -67,8 +69,8 @@ if __name__ == '__main__':
     args = arg_parse()
 
     if args.mode == "sum":
-        print hm2str( sum_time(*args.time) )
+        print(hm2str(sum_time(*args.time)))
 
     elif args.mode == "avg":
         for d in args.divisor:
-            print hm2str( avg_time(args.time, d) )
+            print(hm2str(avg_time(args.time, d)))
